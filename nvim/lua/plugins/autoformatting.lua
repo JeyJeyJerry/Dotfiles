50cf3f1b937d709bf8946ff52f384b4113e7a74d
JeyJeyJerry <jj.sjovall@gmail.com>
2026-09-08 15:56:59 +0300

    update nvim and fastfetch config

--- nvim/lua/plugins/autoformatting.lua
+++ nvim/lua/plugins/autoformatting.lua
@@ -24,7 +24,7 @@ return {
 
     local sources = {
       diagnostics.checkmake,
-      formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown', 'jsonc' } },
+      formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown', 'jsonc', 'toml' } },
       formatting.stylua,
       formatting.shfmt.with { args = { '-i', '4' } },
       formatting.terraform_fmt,
